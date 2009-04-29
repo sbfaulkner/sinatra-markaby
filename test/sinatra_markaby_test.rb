654e41735fcc021fe5682a10485e676331497f4e
S. Brent Faulkner <brentf@unwwwired.net>
2009-04-29 16:09:45 -0400

    test layout support (it works)

--- test/sinatra_markaby_test.rb
+++ test/sinatra_markaby_test.rb
@@ -14,36 +14,45 @@ class SinatraMarkabyTest < Test::Unit::TestCase
   def test_renders_inline_strings
     markaby_app { markaby 'mab.p "Hello shrimp!"' }
     assert last_response.ok?
-    assert_equal "<p>Hello shrimp!</p>", last_response.body
+    assert_equal '<p>Hello shrimp!</p>', last_response.body
   end
 
   def test_renders_inline_blocks
     markaby_app {
-      @name = "Frank & Mary"
+      @name = 'Frank & Mary'
       markaby do |mab|
         mab.p "Hello #{@name}!"
       end
     }
     assert last_response.ok?
-    assert_equal "<p>Hello Frank &amp; Mary!</p>", last_response.body
+    assert_equal '<p>Hello Frank &amp; Mary!</p>', last_response.body
   end
 
   def test_renders_markaby_files_in_views_path
     markaby_app {
-      @name = "World"
+      @name = 'World'
       markaby :hello
     }
     assert last_response.ok?
-    assert_equal "<p>Hello, World!</p>", last_response.body
+    assert_equal '<p>Hello, World!</p>', last_response.body
   end
 
   def test_renders_in_file_template
     markaby_app {
-      @name = "Joe"
+      @name = 'Joe'
       markaby :in_file
     }
     assert last_response.ok?
-    assert_equal "<p>Hey Joe</p>", last_response.body
+    assert_equal '<p>Hey Joe</p>', last_response.body
+  end
+
+  def test_renders_with_layout
+    markaby_app {
+      @name = 'with Layout'
+      markaby :hello, :layout => :html
+    }
+    assert last_response.ok?
+    assert_equal '<html><head><meta content="text/html; charset=utf-8" http-equiv="Content-Type"/><title>Hello</title></head><body><p>Hello, with Layout!</p></body></html>', last_response.body
   end
 
   def test_raises_error_if_template_not_found
